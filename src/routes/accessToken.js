13b2b00b224e6a70e710ea53a8d8f04ada70f0d4
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-10-21 20:13:06 +0100

    Refactor to simplify token handling

--- src/routes/accessToken.js
+++ src/routes/accessToken.js
@@ -1,19 +1,21 @@
 import { Router } from "express";
+import qs from "querystring";
 
 const router = new Router();
 
 router.post("/", (req, res) => {
   const accept = req.get("accept");
-  const accessToken = "e72e16c7e42f292c6912e7710c838347ae178b4a";
-  const tokenType = "bearer";
+
+  const payload = {
+    access_token: "e72e16c7e42f292c6912e7710c838347ae178b4a",
+    token_type: "bearer",
+  };
 
   if (accept === "application/json") {
-    return res.json({
-      access_token: accessToken,
-      token_type: tokenType,
-    });
+    return res.json(payload);
   }
-  res.send(`access_token=${accessToken}&token_type=${tokenType}`);
+
+  res.send(qs.stringify(payload));
 });
 
 export default router;
