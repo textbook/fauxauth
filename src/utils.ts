31c0dfbe0191b67b30135684fae637aad6d66601
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-02-04 20:59:58 +0000

    Extract initial configuration to typed constant

--- src/utils.ts
+++ src/utils.ts
@@ -8,6 +8,13 @@ export interface Configuration {
   codes: string[];
 }
 
+const initialConfiguration: Partial<Configuration> = {
+  accessToken: null,
+  callbackUrl: "http://example.org/",
+  clientId: "1ae9b0ca17e754106b51",
+  clientSecret: "3efb56fdbac1cb21f3d4fea9b70036e04a34d068",
+};
+
 export const generateHex = (length: number) => {
   return Array(length)
     .fill(null)
@@ -15,11 +22,8 @@ export const generateHex = (length: number) => {
     .join("");
 };
 
-export const generateConfiguration: () => Configuration = () => ({
-  accessToken: null,
-  callbackUrl: "http://example.org/",
-  clientId: "1ae9b0ca17e754106b51",
-  clientSecret: "3efb56fdbac1cb21f3d4fea9b70036e04a34d068",
+export const generateConfiguration = (): Configuration => ({
   codes: [],
+  ...initialConfiguration,
   ...JSON.parse(process.env.FAUXAUTH_CONFIG || "{}"),
 });
