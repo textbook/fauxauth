340daca3813e90f77dd49286e10882da4bce2761
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-10-06 11:56:32 +0100

    Use .nvmrc as source of truth

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+NODE_RELEASE="$(cat .nvmrc)"
+
+docker -v
+echo "Node version $NODE_RELEASE"
+
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-docker build . \
+NODE_RELEASE="$NODE_RELEASE" docker build . \
   --label="version=$CIRCLE_TAG" \
   -t 'textbook/fauxauth' \
   -t "textbook/fauxauth:$CIRCLE_TAG"
