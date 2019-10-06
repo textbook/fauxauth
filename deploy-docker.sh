a37bd653d04d0a6ae391ea4a78236657a9f1f05f
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-10-06 14:17:35 +0100

    Fix argument usage in Docker deploy script

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -9,8 +9,9 @@ echo "Node version $NODE_RELEASE"
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-NODE_RELEASE="$NODE_RELEASE" docker build . \
+docker build . \
   --label="version=$CIRCLE_TAG" \
+  --build-arg "NODE_RELEASE=$NODE_RELEASE" \
   -t 'textbook/fauxauth' \
   -t "textbook/fauxauth:$CIRCLE_TAG"
 
