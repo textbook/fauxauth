35825f74911ee1192bff0eed16aaaf0d344769e8
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2019-10-06 14:25:14 +0100

    Fix reference to .nvmrc in the smoke tests
    Also make it cleaner to run them locally.

--- e2e/smoke-test.sh
+++ e2e/smoke-test.sh
@@ -3,14 +3,15 @@
 set -x -e
 
 HERE="$(dirname "$0")"
-NODE_RELEASE="$(cat .nvmrc)"
-
-docker -v
-echo "Node version $NODE_RELEASE"
 
 pushd "$HERE"
+    NODE_RELEASE="$(cat ../.nvmrc)"
+
+    docker -v
+    echo "Node version $NODE_RELEASE"
+
     npm ci
-    npm install "fauxauth@$CIRCLE_TAG"
+    npm install "fauxauth@$CIRCLE_TAG" --no-save
     npm run e2e
     NODE_RELEASE="$NODE_RELEASE" docker-compose run e2e run -d test
 popd
