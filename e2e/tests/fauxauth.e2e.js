3069807249126780a6be7554921963f4fa193146
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-10-28 01:09:06 +0000

    Upgrade to Node 14.14.0 as working version
    Latest supported by circleci/node.
    
    Run E2E tests consistently in latest version, use async syntax.

--- e2e/tests/fauxauth.e2e.js
+++ e2e/tests/fauxauth.e2e.js
@@ -4,48 +4,41 @@ const { format, parse, URLSearchParams } = require("url");
 const baseUrl = process.env.FAUXAUTH_URL || "http://localhost:3000";
 
 describe("fauxauth", () => {
-  beforeEach(() => {
-    return makeRequest("/_configuration", { method: "DELETE" }).then(res => {
-      expect(res.statusCode).toBe(204);
-    });
+  beforeEach(async () => {
+    const { statusCode } = await makeRequest("/_configuration", { method: "DELETE" });
+    expect(statusCode).toBe(204);
   });
 
-  it("works with default configuration", () => {
+  it("works with default configuration", async () => {
     const state = "testing";
 
-    return makeRequest("/authorize", {
+    let res = await makeRequest("/authorize", {
       followRedirect: false,
       qs: { client_id: "1ae9b0ca17e754106b51", state }
-    })
-      .then(res => {
-        expect(res.statusCode).toBe(302);
-        const { query, search, ...url } = parse(res.headers.location, true);
-        expect(format(url)).toBe("http://example.org/");
-        expect(query.state).toBe(state);
-        expect(query.code).toMatch(/[0-9a-f]{20}/);
-        return query.code;
-      })
-      .then(code =>
-        makeRequest("/access_token", {
-          method: "POST",
-          body: new URLSearchParams({
-            client_id: "1ae9b0ca17e754106b51",
-            client_secret: "3efb56fdbac1cb21f3d4fea9b70036e04a34d068",
-            code,
-            state
-          }).toString()
-        })
-      )
-      .then(res => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toMatch(/access_token=[0-9a-f]{40}/);
-      });
+    });
+    expect(res.statusCode).toBe(302);
+    const { query, search, ...url } = parse(res.headers.location, true);
+    expect(format(url)).toBe("http://example.org/");
+    expect(query.state).toBe(state);
+    expect(query.code).toMatch(/[0-9a-f]{20}/);
+
+    res = await makeRequest("/access_token", {
+      method: "POST",
+      body: new URLSearchParams({
+        client_id: "1ae9b0ca17e754106b51",
+        client_secret: "3efb56fdbac1cb21f3d4fea9b70036e04a34d068",
+        code: query.code,
+        state
+      }).toString()
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toMatch(/access_token=[0-9a-f]{40}/);
   });
 
-  it("works with custom configuration", () => {
+  it("works with custom configuration", async () => {
     const code = "deadbeef";
     const token = "somereallylongtoken";
-    return makeRequest("/_configuration", {
+    let res = await makeRequest("/_configuration", {
       body: [
         {
           op: "add",
@@ -57,39 +50,34 @@ describe("fauxauth", () => {
       ],
       json: true,
       method: "PATCH"
-    })
-      .then(res => {
-        expect(res.statusCode).toBe(200);
-        return makeRequest("/access_token", {
-          method: "POST",
-          body: new URLSearchParams({
-            client_id: "1ae9b0ca17e754106b51",
-            client_secret: "notsosecret",
-            code
-          }).toString()
-        });
-      })
-      .then(res => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toContain(`access_token=${token}`);
-      });
+    });
+    expect(res.statusCode).toBe(200);
+
+    res = await makeRequest("/access_token", {
+      method: "POST",
+      body: new URLSearchParams({
+        client_id: "1ae9b0ca17e754106b51",
+        client_secret: "notsosecret",
+        code
+      }).toString()
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toContain(`access_token=${token}`);
   });
 });
 
-const makeRequest = (url, options) => {
-  return axios
-    .request({
-      baseURL: baseUrl,
-      data: options.body,
-      method: options.method,
-      params: options.qs,
-      maxRedirects: options.followRedirect !== false ? 5 : 0,
-      url
-    })
-    .catch(error => error.response)
-    .then(({ data, headers, status }) => ({
-      body: data,
-      headers,
-      statusCode: status
-    }));
-};
+const makeRequest = (url, options) => axios
+  .request({
+    baseURL: baseUrl,
+    data: options.body,
+    method: options.method,
+    params: options.qs,
+    maxRedirects: options.followRedirect !== false ? 5 : 0,
+    url
+  })
+  .catch(error => error.response)
+  .then(({ data, headers, status }) => ({
+    body: data,
+    headers,
+    statusCode: status
+  }));
