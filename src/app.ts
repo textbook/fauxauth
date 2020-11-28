729756dbe21626877b567861a752c2b8c0f17cb6
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-11-28 21:56:35 +0000

    Fix reference to views directory
    In v4.0.0 Express looked for views in the project root, not the fauxauth directory.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import path from "path";
 
 import tokenRouter from "./routes/accessToken";
 import authRouter from "./routes/authorize";
@@ -11,6 +12,7 @@ export default (configuration: Configuration) => {
   const app = express();
 
   app.set("view engine", "ejs");
+  app.set("views", path.join(__dirname, "..", "views"));
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
