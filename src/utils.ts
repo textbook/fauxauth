0510ecd931e8f6e1284d23e9e81ee041f64ab3c4
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-11-30 18:15:31 +0000

    Improve code/token generation

--- src/utils.ts
+++ src/utils.ts
@@ -6,16 +6,17 @@ export interface Configuration {
   tokenMap?: { [role: string]: string };
 }
 
+const hexChars = "0123456789abcdef";
+
 const initialConfiguration: Partial<Configuration> = {
   callbackUrl: "http://example.org/",
   clientId: "1ae9b0ca17e754106b51",
   clientSecret: "3efb56fdbac1cb21f3d4fea9b70036e04a34d068",
 };
 
-export const generateHex = (length: number): string => {
-  const value = Math.random() * (16 ** length - 1);
-  return value.toString(16).padStart(length, "0");
-};
+const choice = (seq: string): string => seq[Math.floor(Math.random() * seq.length)];
+
+export const generateHex = (length: number): string => [...Array(length)].map(() => choice(hexChars)).join("");
 
 export const generateConfiguration = (): Configuration => ({
   codes: {},
--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -33,4 +33,10 @@ describe("generateHex function", () => {
   it("generates unique hex strings", () => {
     expect(generateHex(20)).not.toBe(generateHex(20));
   });
+
+  it("generates hex strings without loads of zeroes", () => {
+    const hex = generateHex(40);
+    const zeroes = Array.from(hex).reduce((count, char) => count + (char === "0" ? 1 : 0), 0);
+    expect(zeroes).toBeLessThan(10);
+  });
 });
