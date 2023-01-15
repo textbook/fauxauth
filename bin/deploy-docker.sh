35087df9f4187ce9dae1f3d918b78529f8ab9bf0
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-01-15 13:43:20 +0000

    Update tag reference in deploy script

--- bin/deploy-docker.sh
+++ bin/deploy-docker.sh
@@ -2,6 +2,14 @@
 
 set -euo pipefail
 
+if [ $# -ne 1 ]; then
+  echo "usage: ./deploy-docker.sh <tag>"
+  echo "This will run the smoke tests for the specified release tag"
+  exit 1
+fi
+
+TAG=$1
+
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 NODE_RELEASE="$(cat "$HERE/../.nvmrc")"
 
@@ -10,14 +18,14 @@ echo "Node version $NODE_RELEASE"
 
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-MAJOR="$(echo "$CIRCLE_TAG" | cut -d. -f1)"
-MINOR="$(echo "$CIRCLE_TAG" | cut -d. -f2)"
+MAJOR="$(echo "$TAG" | cut -d. -f1)"
+MINOR="$(echo "$TAG" | cut -d. -f2)"
 NAME='textbook/fauxauth'
 
 npm run build:docker -- \
-  --label "version=$CIRCLE_TAG" \
+  --label "version=$TAG" \
   --tag "$NAME:$MAJOR" \
   --tag "$NAME:$MAJOR.$MINOR" \
-  --tag "$NAME:$CIRCLE_TAG"
+  --tag "$NAME:$TAG"
 
 docker push "$NAME" --all-tags
